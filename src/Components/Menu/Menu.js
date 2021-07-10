67babd154f3e2e7c3c47d7c916a35c4e1e4d7021
Victor Manuel Delfin Santos <victormanu31611@gmail.com>
2021-07-09 23:11:39 -0500

    get room

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -2,8 +2,8 @@ import React, {useState} from 'react'
 import classes from "./Menu.module.scss"
 
 const Menu = ({getDataUser,leaveGroupHandler})=>{ 
-    const [name, setName] = useState("1");
-    const [room, setRoom] = useState("1");
+    const [name, setName] = useState("");
+    const [room, setRoom] = useState("");
     const [canChange, setCanChange] = useState(true);
 
     const aceptData = () =>{
@@ -25,7 +25,7 @@ const Menu = ({getDataUser,leaveGroupHandler})=>{
         setCanChange(true);
     }
     const getRoomFromServer = () =>{
-        fetch('http://localhost:3000/room')
+        fetch('https://rockpaperscissorsjs.herokuapp.com/room')
         .then(response => response.json())
         .then(data => {
             setRoom(data.room)
